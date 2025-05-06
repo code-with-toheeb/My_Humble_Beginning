2c76910d6c95d1560deae3ee377f18ef5b92ee7e
Toheeb <toheebolawale019@gmail.com>
2025-05-06 10:50:42 +0100

    New code

--- BytePass.py
+++ BytePass.py
@@ -1,19 +1,44 @@
 import random
 import string
 
-user_input1 = int(input("Enter the length of password"))
-user_input2 = str(input("Symbols? choose Y/N")).lower()
-user_input3 = str(input("Digits? choose Y/N")).lower()
-data_used = list(string.ascii_letters)
-sorted_data = []
-
-if user_input2 == "y":
-    data_used += string.punctuation
-if user_input3 == "y":
-    data_used += string.digits
-
-for x in range(0, user_input1):
-    sorted_data.append(random.choice(data_used))
+data_pick = string.ascii_letters
+while True:
+    try:
+        password_length = int(input("Enter the length of password: "))
+        if password_length < 12:
+            print("Enter a Number greater than 11")
+        else:
+            break
+    except ValueError:
+        print("Enter a valid number")
     
 
-print("Password Generated: ","".join(sorted_data))
+
+
+while True:
+        
+        digit_wise = input("Did you want to add  digit to your password Y/N? ").lower()
+        if digit_wise == "y":
+            data_pick += string.digits
+            break
+        elif digit_wise == 'n':
+             rethink = input("Caution! Digit makes passwword stronger. Are you sure you want to proceed Y/N ").lower()
+             if rethink == 'y':
+                  break
+             else:
+                  continue
+             
+while True:
+     character_wise = input("Do you want add character to your password Y/N? ").lower()
+
+     if character_wise == "y":
+          data_pick += string.punctuation
+          break 
+     elif character_wise == 'n':
+          rethink = input("Caution! character makes passwword stronger. Are you sure you want to proceed Y/N ").lower()
+          if rethink == 'y':
+               break
+          else:
+               continue
+
+print(data_pick)
